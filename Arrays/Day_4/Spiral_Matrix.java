28c2c860c931be1f6da270e001429fca285bb048
Vaibhav0710 <Vaibhavjain7171@gmail.com>
2024-07-19 09:40:06 +0530

    day_4

---
+++ Arrays/Day_4/Spiral_Matrix.java
@@ -0,0 +1,29 @@
+package Arrays.Day_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Spiral_Matrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int row = 0;
+        int col = -1;
+        int direction = 1;
+        List<Integer> result = new ArrayList<>();
+        while (rows > 0 && cols > 0) {
+            for (int i = 0; i < cols; i++) {
+                col += direction;
+                result.add(matrix[row][col]);
+            }
+            rows--;
+            for (int i = 0; i < rows; i++) {
+                row += direction;
+                result.add(matrix[row][col]);
+            }
+            cols--;
+            direction *= -1;
+        }
+        return result;
+    }
+}
