a6bdb3760668c013a64fe868a256b1dd0a9e67d1
Vaibhav0710 <Vaibhavjain7171@gmail.com>
2024-07-25 10:29:09 +0530

    day_10

---
+++ String/Day_10/Distinct_Subsequences.java
@@ -0,0 +1,33 @@
+package String.Day_10;
+
+import java.util.Arrays;
+
+public class Distinct_Subsequences {
+    int count = 0;
+
+    private int solveMemo(String s, int idx, String t, int t_idx, int[][] dp) {
+        if (t_idx >= t.length())
+            return 1;
+        if (idx >= s.length())
+            return 0;
+        if (dp[idx][t_idx] != -1) {
+            return dp[idx][t_idx];
+        }
+        int take = 0;
+        int nontake = 0;
+        if (s.charAt(idx) == t.charAt(t_idx)) {
+            take = solveMemo(s, idx + 1, t, t_idx + 1, dp);
+            nontake = solveMemo(s, idx + 1, t, t_idx, dp);
+            return dp[idx][t_idx] = take + nontake;
+        }
+        return dp[idx][t_idx] = solveMemo(s, idx + 1, t, t_idx, dp);
+    }
+
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+        return solveMemo(s, 0, t, 0, dp);
+    }
+}
